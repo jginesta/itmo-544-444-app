f2c8174c5d977a33bf392ccbc81ea4f22e87a5e5
jginesta <jginesta@hawk.iit.edu>
2015-10-02 17:21:23 -0500

    Relative path to user file

--- launch.sh
+++ launch.sh
@@ -12,7 +12,7 @@
 
 
 ###############################################
-aws ec2 run-instances --image-id $1 --count $2 --instance-type $3 --key-name $6 --security-group-ids $4 --subnet-id $5 --associate-public-ip-address --user-data file://~/itmo-544-444-env/install-env.sh --debug
+aws ec2 run-instances --image-id $1 --count $2 --instance-type $3 --key-name $6 --security-group-ids $4 --subnet-id $5 --associate-public-ip-address --user-data file://../itmo-544-444-env/install-env.sh --debug
 
 
 
