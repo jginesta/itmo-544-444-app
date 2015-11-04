118e5f8e6be1d15d6e7af2b0ead77389d3cc072d
jginesta <jginesta@hawk.iit.edu>
2015-11-04 08:27:30 -0600

    Last update1

--- launch-rds.sh
+++ launch-rds.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-
 mapfile -t dbInstanceARR < <(aws rds describe-db-instances --output json | grep "\"DBInstanceIdentifier" | sed "s/[\"\:\, ]//g" | sed "s/DBInstanceIdentifier//g" )
 echo ${#dbInstanceARR[@]}
 
 if [ ${#dbInstanceARR[@]} -gt 0 ]
    then
-   echo "Deleting existing RDS database-instances"
+  # echo "Deleting existing RDS database-instances"
    LENGTH=${#dbInstanceARR[@]}
 
       for (( i=0; i<=${LENGTH}; i++));
@@ -15,17 +14,17 @@ if [ ${#dbInstanceARR[@]} -gt 0 ]
      then 
       echo "db exists"
      else
-      aws rds create-db-subnet-group --db-subnet-group-name itmo544-mp1-sgn  --subnet-ids subnet-e42819cf subnet-140de262 --db-subnet-group-description "itmosg-jgl"
+      sudo aws rds create-db-subnet-group --db-subnet-group-name itmo544-mp1-sgn  --subnet-ids subnet-e42819cf subnet-140de262 --db-subnet-group-description "itmosg-jgl"
 
       sudo aws rds create-db-instance --db-name customerrecords --db-instance-identifier mp1-jgl --db-instance-class db.t1.micro --engine MySQL --master-username controller --master-user-password letmein888 --allocated-storage 5 --vpc-security-group-ids sg-6e7a9708 --db-subnet-group-name itmo544-mp1-sgn --publicly-accessible
-     
+      fi  
       sudo aws rds wait db-instance-available --db-instance-identifier mp1-jgl
     # rds-describe-db-instances
      #$link = mysqli_connect($endpoint,"controller","letmein888","customerrecords"); 
 
 #--subnet-ids subnet-e42819cf subnet-140de262
    
-      fi  
+    
      done
 fi
 sudo apt-get install php5-cli php5-mysql
