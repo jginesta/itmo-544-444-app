c000bf32ad36cf02cfbe71530868714852739d70
jginesta <jginesta@hawk.iit.edu>
2015-11-03 22:34:48 -0600

    sudo included

--- launch-rds.sh
+++ launch-rds.sh
@@ -15,11 +15,11 @@ if [ ${#dbInstanceARR[@]} -gt 0 ]
      then 
       echo "db exists"
      else
-     aws rds create-db-subnet-group --db-subnet-group-name itmo544-mp1-sgn  --subnet-ids subnet-e42819cf subnet-140de262 --db-subnet-group-description "itmosg-jgl"
+      aws rds create-db-subnet-group --db-subnet-group-name itmo544-mp1-sgn  --subnet-ids subnet-e42819cf subnet-140de262 --db-subnet-group-description "itmosg-jgl"
 
-     aws rds create-db-instance --db-name customerrecords --db-instance-identifier mp1-jgls --db-instance-class db.t1.micro --engine MySQL --master-username controller --master-user-password letmein888 --allocated-storage 5 --vpc-security-group-ids sg-6e7a9708 --db-subnet-group-name itmo544-mp1-sgn --publicly-accessible
+      sudo aws rds create-db-instance --db-name customerrecords --db-instance-identifier mp1-jgl --db-instance-class db.t1.micro --engine MySQL --master-username controller --master-user-password letmein888 --allocated-storage 5 --vpc-security-group-ids sg-6e7a9708 --db-subnet-group-name itmo544-mp1-sgn --publicly-accessible
      
-     aws rds wait db-instance-available --db-instance-identifier mp1-jgls
+      sudo aws rds wait db-instance-available --db-instance-identifier mp1-jgl
     # rds-describe-db-instances
      #$link = mysqli_connect($endpoint,"controller","letmein888","customerrecords"); 
 
