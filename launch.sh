a7375655a9ae5bf28e2591954ebf71a4d4fcb2ae
jginesta <jginesta@hawk.iit.edu>
2015-11-05 14:57:09 -0600

    install mysql

--- launch.sh
+++ launch.sh
@@ -31,9 +31,6 @@ aws elb register-instances-with-load-balancer  --load-balancer-name itmo544-jgl-
 
 aws elb configure-health-check --load-balancer-name itmo544-jgl-lb --health-check Target=HTTP:80/index.html,Interval=30,UnhealthyThreshold=2,HealthyThreshold=2,Timeout=3
 
-echo -e "\nWaiting for 3:30 minutes for LB to create before opening in the web browser"
-for i in {0..210}; do echo -ne '.';sleep 1;done
-
 #aws elb create-lb-cookie-stickiness-policy --load-balancer-name itmo544-jgl-lb --policy-name my-duration-cookie-policy --cookie-expiration-period 60
 
 #Creating a launch configuration
@@ -44,9 +41,24 @@ aws autoscaling put-scaling-policy --auto-scaling-group-name itmo-544-extended-a
 
 aws cloudwatch put-metric-alarm --alarm-name cpugreaterthan30 --alarm-description "Alarm when CPU exceeds 30 percent" --metric-name CPUUtilization --namespace AWS/EC2 --statistic Average --period 300 --threshold 30 --comparison-operator GreaterThanOrEqualToThreshold  --dimensions Name=itmo-544-extended-auto-scaling-group-2,Value=itmo-544-extended-auto-scaling-group-2 --evaluation-periods 2 --alarm-actions arn:aws:sns:us-east-1:111122223333:MyTopic --unit Percent
 aws cloudwatch put-metric-alarm --alarm-name cpulessthan10 --alarm-description "Alarm when CPU is less than 10 percent" --metric-name CPUUtilization --namespace AWS/EC2 --statistic Average --period 300 --threshold 10 --comparison-operator LessThanOrEqualToThreshold  --dimensions Name=itmo-544-extended-auto-scaling-group-2,Value=itmo-544-extended-auto-scaling-group-2 --evaluation-periods 2 --alarm-actions arn:aws:sns:us-east-1:111122223333:MyTopic --unit Percent
-./launch-rds.sh
+#./launch-rds.sh
+
+sudo aws rds create-db-subnet-group --db-subnet-group-name itmo544-mp1-sgn  --subnet-ids subnet-e42819cf subnet-140de262 --db-subnet-group-description "itmosg-jgl"
+
+sudo aws rds create-db-instance --db-name customerrecords --db-instance-identifier mp1-jgl --db-instance-class db.t1.micro --engine MySQL --master-username controller --master-user-password letmein888 --allocated-storage 5 --vpc-security-group-ids sg-6e7a9708 --db-subnet-group-name itmo544-mp1-sgn --publicly-accessible
+
+sudo aws rds wait db-instance-available --db-instance-identifier mp1-jgl
+echo "db instance created"         
+
+sudo aws rds create-db-instance-read-replica --db-instance-identifier mp1-jgl-read --source-db-instance-identifier mp1-jgl --publicly-accessible
+echo "read replica created"  
+
+sudo php ../itmo544-444-fall2015/setup-lite.php
+echo "db table created"  
+
+echo -e "\nWaiting for 3:30 minutes for LB to create before opening in the web browser"
+for i in {0..210}; do echo -ne '.';sleep 1;done
+
 firefox $LoadBalancerURL &
-export LoadBalancerURL
 
-
-
+echo -e "Everything worked correctly"
